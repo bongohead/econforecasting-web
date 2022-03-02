e20679696c5d8afaa159040c81ea78d2a861728b
Charles Ye <charles@cmefi.com>
2022-03-02 01:31:06 -0500

    Updated homepage & autocomplete (v0.20)

--- js/home.js
+++ js/home.js
@@ -1,16 +1,23 @@
 window.addEventListener('DOMContentLoaded', () => {
 	
 	const page_list = [
-		{group: 'Interest Rates', fullname: 'Federal Funds Rate (FFR)', url: 'rates-model-ffr'},
-		{group: 'Interest Rates', fullname: 'Secured Overnight Financing Rate (SOFR)', url: 'rates-model-sofr'},
-		{group: 'Interest Rates', fullname: '3-Month Treasury Yield', url: 'rates-model-treasury-3m'},
-		{group: 'Interest Rates', fullname: '6-Month Treasury Yield', url: 'rates-model-treasury-6m'},
-		{group: 'Interest Rates', fullname: '1-Year Treasury Yield', url: 'rates-model-treasury-1y'},
-		{group: 'Interest Rates', fullname: '2-Year Treasury Yield', url: 'rates-model-treasury-2y'},
-		{group: 'Interest Rates', fullname: '5-Year Treasury Yield', url: 'rates-model-treasury-5y'},
-		{group: 'Interest Rates', fullname: '10-Year Treasury Yield', url: 'rates-model-treasury-10y'},
-		{group: 'Interest Rates', fullname: '20-Year Treasury Yield', url: 'rates-model-treasury-20y'},
-		{group: 'Interest Rates', fullname: '30-Year Treasury Yield', url: 'rates-model-treasury-30y'}
+		{group: 'Forecast', fullname: 'Federal Funds Rate (FFR)', url: 'forecast-ffr'},
+		{group: 'Forecast', fullname: 'Secured Overnight Financing Rate (SOFR)', url: 'forecast-sofr'},
+		{group: 'Forecast', fullname: '3-Month Treasury Yield', url: 'forecast-t03m'},
+		{group: 'Forecast', fullname: '6-Month Treasury Yield', url: 'forecast-t06m'},
+		{group: 'Forecast', fullname: '1-Year Treasury Yield', url: 'forecast-t01y'},
+		{group: 'Forecast', fullname: '2-Year Treasury Yield', url: 'forecast-t02y'},
+		{group: 'Forecast', fullname: '5-Year Treasury Yield', url: 'forecast-t05y'},
+		{group: 'Forecast', fullname: '10-Year Treasury Yield', url: 'forecast-t10y'},
+		{group: 'Forecast', fullname: '20-Year Treasury Yield', url: 'forecast-t20y'},
+		{group: 'Forecast', fullname: '30-Year Treasury Yield', url: 'forecast-t30y'},
+		{group: 'Forecast', fullname: '30-Year Mortgage Rate', url: 'forecast-mort30y'},
+		{group: 'Forecast', fullname: '15-Year Mortgage Rate', url: 'forecast-mort15y'},
+		{group: 'Forecast', fullname: 'BSBY Rate', url: 'forecast-bsby'},
+		{group: 'Forecast', fullname: 'AMERIBOR', url: 'forecast-ameribor'},
+		{group: 'US Economy', fullname: 'Gross Domestic Product (GDP)', url: 'forecast-gdp'},
+		{group: 'US Economy', fullname: 'Personal Consumption (PCE)', url: 'forecast-pce'},
+		{group: 'US Economy', fullname: 'CPI Inflation', url: 'forecast-cpi'}
 	];
 	
 	// API Basic Configuration Object
@@ -18,8 +25,9 @@ window.addEventListener('DOMContentLoaded', () => {
 		placeHolder: 'Type an variable name (e.g. "Treasury Yield")',
 		data: {
 			cache: false,
-			keys: [...new Set(page_list.map(x => x.group))],
-			src: page_list.map(x => ({[x.group]: x.fullname}))
+			// Weird bug - can't be empty 
+			src: page_list.map(x => ({'Forecast': 'poop', 'US Economy' : 'poop', ...{[x.group]: x.fullname}})),
+			keys: [...new Set(page_list.map(x => x.group))]
 		},
 		wrapper: false,
 		resultsList: {
@@ -39,12 +47,14 @@ window.addEventListener('DOMContentLoaded', () => {
 				<span style="display: flex; align-items: center; font-size: 13px; font-weight: 100; text-transform: uppercase; color: rgba(0,0,0,.2);">
 					${data.key}
 				</span>`;
+				
 			},
 			highlight: {
 				render: true
 			}
 		}
-	}
+	};
+	console.log(autocomplete_config);
 	
 	const autoCompleteJS = new autoComplete(autocomplete_config);
 	
@@ -58,12 +68,12 @@ window.addEventListener('DOMContentLoaded', () => {
 	
 	document.querySelector("#autoComplete").addEventListener("close", function (event) {
 		// "event.detail" carries the autoComplete.js "feedback" object
-		console.log(event.detail);
+		// console.log(event.detail);
 		autoCompleteJS.input.value = null;
 	});
 	
 	document.querySelector("#autoComplete").addEventListener("click", function (event) {
-		console.log(autoCompleteJS.input.value);
+		//console.log(autoCompleteJS.input.value);
 		autoCompleteJS.start(' ');
 		autoCompleteJS.input.value = null;
 	});
@@ -73,5 +83,9 @@ window.addEventListener('DOMContentLoaded', () => {
 }); 
 
 const getDir = function(page_obj) {
-	console.log(page_obj);
+	// Simulate an HTTP redirect:
+	console.log('clicked', page_obj);
+
+	window.location.replace(page_obj.url);
+	//window.location.href = page_obj.url
 }
